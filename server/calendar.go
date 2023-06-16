268debc4bf1d0b5ba5aa6c447c3ed8627cd89f8d
nich1 <91976504+nich1@users.noreply.github.com>
2023-06-15 19:34:18 -0500

    Changed the structs to enable a more efficient system for handling reminders. Updated DELETE and POST to properly integrate with system time listener

--- server/calendar.go
+++ server/calendar.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"reflect"
 
 	"github.com/gin-gonic/gin"
@@ -53,7 +54,6 @@ func getCalendar(context *gin.Context) {
 
 }
 
-// TODO VERY IMPORTANT ADD REMINDERS HERE
 func addCalendar(context *gin.Context) {
 	// Read in database file for calendar
 	fileContent, fileErr := ioutil.ReadFile(calendarFile)
@@ -93,10 +93,81 @@ func addCalendar(context *gin.Context) {
 		fmt.Println(writeErr)
 	}
 
-	// TODO VERY IMPORTANT ADD REMINDERS HERE
+	// TODO VERY IMPORTANT ADD REMINDERS HERE- needs to be tested now
+
+	// Add payloads date and reminders to an array,
+	// add dates to server calendar
+	var eventQueue []timeInstance
+	for i := 0; i < len(eventQueue); i++ {
+		// Open / create proper directories and file
+		var eventYear string
+		if eventQueue[i].annual == true {
+			eventYear = "annual"
+		} else {
+			eventYear = string(eventQueue[i].year)
+		}
+		if _, dirErr1 := os.Stat("database/time/calendar/" + eventYear); dirErr1 != nil {
+			if os.IsNotExist(dirErr1) {
+				// Dir does not exist, create year dir
+				dirErr2 := os.Mkdir(("database/time/calendar/" + eventYear), 0755)
+				if dirErr2 != nil {
+					fmt.Println("Error in making directory")
+				}
+			}
+		}
+		var eventMonth string
+		eventMonth = string(eventQueue[i].month)
+		if _, dirErr3 := os.Stat("database/time/calendar/" + eventYear + "/" + eventMonth); dirErr3 != nil {
+			if os.IsNotExist(dirErr3) {
+				// Dir does not exist, create month dir
+				dirErr4 := os.Mkdir(("database/time/calendar/" + eventYear + "/" + eventMonth), 0755)
+				if dirErr4 != nil {
+					fmt.Println("Error in making directory")
+				}
+			}
+		}
+		var filename string
+		filename = string(eventQueue[i].day) + ".json"
+		if _, fileErr1 := os.Stat("database/time/calendar/" + eventYear + "/" + eventMonth + "/" + filename); fileErr1 != nil {
+			if os.IsNotExist(fileErr1) {
+				// File does not exist, create file
+				_, fileErr2 := os.Create(("database/time/calendar/" + eventYear + "/" + eventMonth + "/" + filename))
+				if fileErr2 != nil {
+					fmt.Println("Error in making file")
+				}
+			}
+		}
+		// Read in targeted file for calendar
+		fileContent, fileErr := ioutil.ReadFile("database/time/calendar/" + eventYear + "/" + eventMonth + "/" + filename)
+		if fileErr != nil {
+			fmt.Println(fileErr)
+			return
+		}
+
+		// Convert file into an array of calendarEvent
+		var eventArray []timeInstance
+		unmarshalErr1 := json.Unmarshal([]byte(fileContent), &eventArray)
+		if unmarshalErr1 != nil {
+			fmt.Println(unmarshalErr1)
+			return
+		}
+		eventArray = append(eventArray, eventQueue[i])
+
+		// Convert array back to json
+		writeJson2, writeJsonErr := json.Marshal(eventArray)
+		if writeJsonErr != nil {
+			fmt.Println(writeJsonErr)
+		}
+
+		// Write eventArray to schedules.json file
+		writeErr := ioutil.WriteFile("database/time/calendar/"+eventYear+"/"+eventMonth+"/"+filename, writeJson2, 0644)
+		if writeErr != nil {
+			fmt.Println(writeErr)
+		}
+	}
+
 }
 
-// TODO VERY IMPORTANT ADD REMINDERS HERE
 func deleteCalendar(context *gin.Context) {
 	// Read in database file for calendar
 	fileContent, fileErr := ioutil.ReadFile(calendarFile)
@@ -139,10 +210,65 @@ func deleteCalendar(context *gin.Context) {
 	if writeErr != nil {
 		fmt.Println(writeErr)
 	}
-	// TODO VERY IMPORTANT ADD REMINDERS HERE
+
+	// TODO VERY IMPORTANT ADD REMINDERS HERE- needs to be tested now
+
+	// Add payloads date and reminders to an array,
+	// removes dates from server calendar
+	var eventQueue []timeInstance
+	for i := 0; i < len(eventQueue); i++ {
+		var eventYear string
+		if eventQueue[i].annual == true {
+			eventYear = "annual"
+		} else {
+			eventYear = string(eventQueue[i].year)
+		}
+		var filename string
+		filename = string(eventQueue[i].day) + ".json"
+		// Read in targeted file for calendar
+		fileContent, fileErr := ioutil.ReadFile("database/time/calendar/" + eventYear + "/" + string(eventQueue[i].month) + "/" + filename)
+		if fileErr != nil {
+			fmt.Println(fileErr)
+			return
+		}
+
+		// Convert file into an array of calendarEvent
+		var eventArray []timeInstance
+		unmarshalErr1 := json.Unmarshal([]byte(fileContent), &eventArray)
+		if unmarshalErr1 != nil {
+			fmt.Println(unmarshalErr1)
+			return
+		}
+		for j := 0; j < len(eventArray); j++ {
+			if eventQueue[i] == eventArray[j] {
+				eventArray = append(eventArray[:j], eventArray[j+1:]...)
+				break
+			}
+		}
+
+		// Convert array back to json
+		writeJson2, writeJsonErr := json.Marshal(eventArray)
+		if writeJsonErr != nil {
+			fmt.Println(writeJsonErr)
+		}
+
+		// Write eventArray to schedules.json file
+		writeErr := ioutil.WriteFile("database/time/calendar/"+eventYear+string(eventQueue[i].month)+"/"+filename, writeJson2, 0644)
+		if writeErr != nil {
+			fmt.Println(writeErr)
+		}
+	}
 }
 
-// TODO VERY IMPORTANT ADD REMINDERS HERE
+func putCalendar(context *gin.Context) {
+	// Create two arrays for comparison
+	var oldArray []timeInstance
+	var newArray []timeInstance
+	oldArray = append(oldArray)
+
+}
+
+/*
 func putCalendar(context *gin.Context) {
 	// Assumes that an array is passed in. The first object in the array (both are calendarEvent struct-able)
 	// is the object to be replaced, and the second is the new object data
@@ -172,7 +298,7 @@ func putCalendar(context *gin.Context) {
 
 	// Change matching object in schedulesArray to the new data
 	for i := 0; i < len(calendarArray); i++ {
-		if reflect.DeepEqual(convertedJson[0], calendarArray[i]) /*convertedJson[0] == calendarArray[i]*/ {
+		if reflect.DeepEqual(convertedJson[0], calendarArray[i]) /*convertedJson[0] == calendarArray[i] {
 			calendarArray[i] = convertedJson[1]
 		}
 	}
@@ -190,3 +316,4 @@ func putCalendar(context *gin.Context) {
 	}
 	// TODO VERY IMPORTANT ADD REMINDERS HERE
 }
+*/
